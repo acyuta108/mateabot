ac5b52a736c63bb5074850c18e3f465f26c8b45f
Acyuta <acyuta108@gmail.com>
2016-04-29 23:25:28 -0400

    Fix bug in server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ controller.setupWebserver(port, function (err, webserver) {
 })
 
 controller.hears(['hello', 'hi'], 'message_received', function (bot, message) {
-  bot.reply(message, 'Namaste! Sure let\'s check the schedule for you..)
+  bot.reply(message, 'Namaste! Sure let\'s check the schedule for you..')
   bot.reply(message, {
     attachment: {
       type: 'template',
